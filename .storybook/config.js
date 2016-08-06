b241aed42c5e406f96f611c3a37592b084785654
Lucas Reis <lucasmreis@gmail.com>
2016-08-06 15:04:52 -0300

    storybook ok with custom css!

---
+++ .storybook/config.js
@@ -0,0 +1,13 @@
+import { configure } from '@kadira/storybook';
+
+// Isso coloca automaticamente todos os arquivos
+// com nome *-story.js no Storybook.
+// Referência: https://github.com/kadirahq/react-storybook/blob/master/docs/configure_storybook.md
+
+const req = require.context('../app', true, /-story\.js$/)
+
+function loadStories() {
+  req.keys().forEach(req)
+}
+
+configure(loadStories, module);
--- app/televendas/televendas-story.js
+++ app/televendas/televendas-story.js
@@ -1,7 +1,12 @@
+import React from 'react';
+import { storiesOf, action } from '@kadira/storybook';
 
+import Televendas from './televendas-component'
 
-
-
-//
-// STORYBOOK STORIES !!!
-//
+storiesOf('Televendas', module)
+  .add('with status', () => (
+    <Televendas sessionStatus='some status' startNewSession={action('started new session')} />
+  ))
+  .add('with no status', () => (
+    <Televendas startNewSession={action('started new session')} />
+  ));
