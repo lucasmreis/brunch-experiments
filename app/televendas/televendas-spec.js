af16dbdb95ac051c513da5b720107d5cca4479df
Lucas Reis <lucasmreis@gmail.com>
2016-08-06 14:05:51 -0300

    fix spacing

--- app/televendas/televendas-spec.js
+++ app/televendas/televendas-spec.js
@@ -6,9 +6,7 @@ describe('televendas', function() {
   it('startNewSession', function() {
     const state = { session: 'old session' }
     Object.freeze(state) // to be sure the function will not mutate the state!
-
     const expected = { session: 'new session' }
-
     const action = startNewSession()
 
     assert.deepEqual(reducer(state, action), expected)
